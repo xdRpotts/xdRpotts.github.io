c2f5d43b52de843f46ea4c57f5f1f371e7e26977
xdRpotts <rpotts1604@gmail.com>
2022-03-27 21:57:11 +0100

    added upgrade

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
 
 var number = 10
 var amount = 0
+var clickUpgrade = 1
 
 function clicker(){
+    var btn = document.getElementById("mybtn");
+    btn.value = number;
+    btn.innerHTML = number;
+    number = number - clickUpgrade
     if (number < 1){
         number = 10;
         amount = (amount + 1)
         document.getElementById("amountOf").innerHTML = amount
     }
-    var btn = document.getElementById("mybtn");
-    btn.value = number;
-    btn.innerHTML = number;
-    number = number - 1;
 
+}
+
+function upgrade(){
+    if (amount >= 15){
+        clickUpgrade = (clickUpgrade + 1)
+        amount = (amount - 15)
+        document.getElementById("amountOf").innerHTML = amount
+    }
+    else if (amount < 15){
+        clickUpgrade = (clickUpgrade + 0)
+    }
 }
