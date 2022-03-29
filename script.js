814e8e164cade3d1d8a1f5b63367ca848366d2ce
xdRpotts <rpotts1604@gmail.com>
2022-03-29 23:19:19 +0100

    wip progression

--- script.js
+++ script.js
@@ -1,28 +1,36 @@
 
-var number = 10
-var amount = 0
-var clickUpgrade = 1
+var number = 10;
+var amount = 0;
+var clickUpgrade = 1;
+var numberConstant = 10
+
 
 function clicker(){
     var btnOne = document.getElementById("mybtn");
     btnOne.value = number;
     btnOne.innerHTML = number;
-    number = number - clickUpgrade
+    number = number - clickUpgrade;
     if (number < 1){
-        number = 10;
-        amount = (amount + 1)
-        document.getElementById("amountOf").innerHTML = amount
+        number = numberConstant;
+        amount = (amount + 1);
+        document.getElementById("amountOf").innerHTML = amount;
     }
     
 }
 
 function upgrade(){
     if (amount >= 15){
-        clickUpgrade = (clickUpgrade + 1)
-        amount = (amount - 15)
-        document.getElementById("amountOf").innerHTML = amount
+        clickUpgrade = (clickUpgrade + 1);
+        amount = (amount - 15);
+        document.getElementById("amountOf").innerHTML = amount;
     }
     else if (amount < 15){
         clickUpgrade = (clickUpgrade + 0)
+    }
+    if (clickUpgrade > 10 || clickUpgrade < 10){
+        number = number + 0;
+    }
+    else if (clickUpgrade == 10){
+        numberConstant = 20;
     }
 }
